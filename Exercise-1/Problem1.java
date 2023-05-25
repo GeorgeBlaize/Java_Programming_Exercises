791c7d45b62265e2f737a26af451b8340ea0d793
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-05-26 02:57:23 +0600

    Update Problem1.java

--- Exercise-1/Problem1.java
+++ Exercise-1/Problem1.java
@@ -1,1 +1,17 @@
 
+//Java Program to Print an Integer (Entered by the User)
+
+import java.util.Scanner;
+
+public class Problem1 {
+    public static void main(String[] args) {
+
+        System.out.print("Enter a number: ");
+        Scanner input= new Scanner(System.in);
+
+        int num= input.nextInt();
+
+        System.out.println("You entered: "+num);
+    }
+
+}

74bdb22d3729e54f448326c03b72fc0fb54f1b62
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-05-26 03:01:18 +0600

    Add files via upload

---
+++ Exercise-1/Problem2.java
@@ -0,0 +1,13 @@
+//Java Program to Add Two Integers
+
+public class Problem2 {
+    public static void main(String[] args) {
+        System.out.println("Enter two numbers");
+        int firstNum=10;
+        int secondNum=20;
+
+        int sum=firstNum+secondNum;
+
+        System.out.println("The sum is: "+sum);
+    }
+}

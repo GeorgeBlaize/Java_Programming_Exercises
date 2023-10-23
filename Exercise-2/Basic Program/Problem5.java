0cb45a319d4825c4f7356bca0d182d0fe1454a4b
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-10-24 00:55:39 +0600

    Create Problem5.java

---
+++ Exercise-2/Basic Program/Problem5.java
@@ -0,0 +1,23 @@
+package Basic_Programs;
+
+//Sum of two numbers from user input
+
+import java.util.Scanner;
+
+public class Problem5 {
+
+    public static void main(String[] args) {
+
+        int num1,num2,sum;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter First Number:");
+        num1=sc.nextInt();
+
+        System.out.println("Enter Second Number:");
+        num2=sc.nextInt();
+
+        sc.close();
+        sum=num1+num2;
+        System.out.println("Sum of these numbers: "+sum);
+    }
+}

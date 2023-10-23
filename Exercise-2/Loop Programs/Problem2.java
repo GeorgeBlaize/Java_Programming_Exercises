16e83f97f95670323b2cbe3e9f1eceda806503d1
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-10-24 01:40:58 +0600

    Create Problem2.java

---
+++ Exercise-2/Loop Programs/Problem2.java
@@ -0,0 +1,16 @@
+package Loop_Programs;
+
+//Factorial Program
+
+public class Problem2 {
+
+    public static void main(String[] args) {
+
+        int number=5;
+        long fact=1;
+        for (int i=1; i<=number; i++){
+            fact *= i;
+        }
+        System.out.println("Factorial of "+number+" is: "+fact);
+    }
+}

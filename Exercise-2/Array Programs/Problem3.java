816c39f25e47e8419cfc9a6b3bd323d542d21ad7
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-10-26 14:26:04 +0600

    Create Problem3.java

---
+++ Exercise-2/Array Programs/Problem3.java
@@ -0,0 +1,38 @@
+package Array_Programs;
+
+//Student grade average
+
+import java.util.Scanner;
+
+public class Problem3 {
+
+    public static void main(String[] args) {
+
+        int marks[]=new int[6];
+        int i;
+        float total=0,avg;
+        Scanner scanner=new Scanner(System.in);
+
+        for(i=0; i<6; i++){
+            System.out.println("Enter Marks of Subject"+(i+1)+".");
+            marks[i]=scanner.nextInt();
+            total=total+marks[i];
+        }
+        scanner.close();
+        avg=total/6;
+        System.out.println("The student Grade is: ");
+
+        if(avg>=80){
+            System.out.println("A");
+        }
+        else if(avg>=60 && avg<80){
+            System.out.println("B");
+        }
+        else if(avg>=40 && avg<60){
+            System.out.println("C");
+        }
+        else {
+            System.out.println("D");
+        }
+    }
+}
